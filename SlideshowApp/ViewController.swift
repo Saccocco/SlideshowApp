777d2f0cd812daf00e6f1b8f3cbff501fdbe745d
sachiko.nakagiri <sachikonakagiri@gmail.com>
2020-08-29 21:12:14 +0200

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Sachiko Nakagiri on 2020/08/29.
+//  Copyright © 2020 sachiko.nakagiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
